6e33d8633f6a918377bde75d2d23f79e99f295ed
fratertamas <fratertomi@gmail.com>
2023-11-15 16:02:23 +0100

    Add field and constructor to ExamService class

--- src/main/java/nye/rft/service/ExamService.java
+++ src/main/java/nye/rft/service/ExamService.java
@@ -1,4 +1,11 @@
 package nye.rft.service;
 
+import nye.rft.repository.ExamRepository;
+
 public class ExamService {
+    private ExamRepository examRepository;
+
+    public ExamService(ExamRepository examRepository) {
+        this.examRepository = examRepository;
+    }
 }

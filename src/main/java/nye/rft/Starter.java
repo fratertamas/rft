5c85bfb89e459abef718bc1225cab3f0b6325113
fratertamas <fratertomi@gmail.com>
2023-11-15 16:45:43 +0100

    Starter class has been moved to the 'nye.rft' package and implemented.

---
+++ src/main/java/nye/rft/Starter.java
@@ -0,0 +1,11 @@
+package nye.rft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Starter {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Starter.class, args);
+    }
+}

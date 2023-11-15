4a3e9e7c3f8a1eef51ddb10360a1c4bab8ada0b2
fratertamas <fratertomi@gmail.com>
2023-11-15 16:51:40 +0100

    Add annotations to ExamController for RESTful API endpoints and implement getAllExams method

--- src/main/java/nye/rft/controller/ExamController.java
+++ src/main/java/nye/rft/controller/ExamController.java
@@ -3,19 +3,30 @@ package nye.rft.controller;
 import nye.rft.model.Exam;
 import nye.rft.model.User;
 import nye.rft.service.ExamService;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/exams")
 public class ExamController {
     private final ExamService examService;
 
     public ExamController(ExamService examService) {
         this.examService = examService;
     }
-
-    public void createExam(Exam exam) {
+    @PostMapping
+    public void createExam(@RequestBody Exam exam) {
         examService.createExam(exam);
     }
 
-    public void registerStudentForExam(String examId, User user) {
+    @GetMapping
+    public List<Exam> getAllExams() {
+        return examService.getAllExams();
+    }
+
+    @PostMapping("/{examId}")
+    public void registerStudentForExam(@PathVariable("examId") String examId, @RequestBody User user) {
         examService.registerStudentForExam(examId, user);
     }
 }

1051e595bb8fc3163f61a846b69e8055694099a6
fratertamas <fratertomi@gmail.com>
2023-11-15 14:16:30 +0100

    Add fields and constructor to User class

--- src/main/java/nye/rft/model/User.java
+++ src/main/java/nye/rft/model/User.java
@@ -1,4 +1,17 @@
 package nye.rft.model;
 
 public class User {
+    private String id;
+    private String name;
+    private UserRole role;
+
+    public User() {
+    }
+
+    public User(String id, String name, UserRole role) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+    }
+
 }

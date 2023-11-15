5198e332f8c0672eb034cfc805ce3f36018a3b83
fratertamas <fratertomi@gmail.com>
2023-11-15 15:58:38 +0100

    Add field and methods to ExamRepository class

--- src/main/java/nye/rft/repository/ExamRepository.java
+++ src/main/java/nye/rft/repository/ExamRepository.java
@@ -1,4 +1,19 @@
 package nye.rft.repository;
 
+import nye.rft.model.Exam;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExamRepository {
+
+    private List<Exam> exams = new ArrayList<>();
+
+    public void saveExam(Exam exam) {
+        exams.add(exam);
+    }
+
+    public List<Exam> getAllExams() {
+        return exams;
+    }
 }

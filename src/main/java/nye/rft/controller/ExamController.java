8a1c35da4df1312405f0218981b1ff9107f94065
fratertamas <fratertomi@gmail.com>
2023-11-15 16:05:31 +0100

    Add field and constructor to ExamController class

--- src/main/java/nye/rft/controller/ExamController.java
+++ src/main/java/nye/rft/controller/ExamController.java
@@ -1,4 +1,11 @@
 package nye.rft.controller;
 
+import nye.rft.service.ExamService;
+
 public class ExamController {
+    private final ExamService examService;
+
+    public ExamController(ExamService examService) {
+        this.examService = examService;
+    }
 }

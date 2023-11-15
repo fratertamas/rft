570f0352befe9f61695679ed0d3c392eead32b6b
fratertamas <fratertomi@gmail.com>
2023-11-15 16:03:04 +0100

    Add methods to ExamService class

--- src/main/java/nye/rft/service/ExamService.java
+++ src/main/java/nye/rft/service/ExamService.java
@@ -1,11 +1,22 @@
 package nye.rft.service;
 
+import nye.rft.model.Exam;
 import nye.rft.repository.ExamRepository;
+
+import java.util.List;
 
 public class ExamService {
     private ExamRepository examRepository;
 
     public ExamService(ExamRepository examRepository) {
         this.examRepository = examRepository;
+    }
+
+    public void createExam(Exam exam) {
+        examRepository.saveExam(exam);
+    }
+
+    public List<Exam> getAllExams() {
+        return examRepository.getAllExams();
     }
 }

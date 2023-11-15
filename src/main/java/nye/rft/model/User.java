3a4a4533e5183062fa858e90ca112b3d5284b8e4
fratertamas <fratertomi@gmail.com>
2023-11-15 14:18:29 +0100

    Add setter and getter methods in User class

--- src/main/java/nye/rft/model/User.java
+++ src/main/java/nye/rft/model/User.java
@@ -13,5 +13,27 @@ public class User {
         this.name = name;
         this.role = role;
     }
+    public String getId() {
+        return id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
 }

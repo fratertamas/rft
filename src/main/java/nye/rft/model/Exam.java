73c238bd4d7658b25b4390828dbc24b3bb6370d6
fratertamas <fratertomi@gmail.com>
2023-11-15 14:27:39 +0100

    Add fields and constructor to Exam class

--- src/main/java/nye/rft/model/Exam.java
+++ src/main/java/nye/rft/model/Exam.java
@@ -1,4 +1,25 @@
 package nye.rft.model;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Exam {
+    private String id;
+    private Date date;
+    private String course;
+    private String location;
+    private User user;
+    private int maxStudents;
+    private List<User> registeredStudents;
+
+    public Exam(String id, Date date, String course, String location, User user, int maxStudents) {
+        this.id = id;
+        this.date = date;
+        this.course = course;
+        this.location = location;
+        this.user = user;
+        this.maxStudents = maxStudents;
+        this.registeredStudents = new ArrayList<>();
+    }
 }
